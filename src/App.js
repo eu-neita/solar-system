71a0eac6ce5725441448d151347f07da6914d0ba
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-03-01 17:11:42 -0300

    formata

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
-import Missioncard from './components/MissionsCard';
 
 class app extends React.Component {
   render() {
@@ -11,7 +10,6 @@ class app extends React.Component {
         <Header />
         <SolarSystem />
         <Missions />
-        <Missioncard />
       </div>
     );
   }
